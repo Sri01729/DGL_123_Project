b5be61f0b11755cfaee805add2f99ac1a650beff
Sri01729 <salahari@northislandcollege.ca>
2023-10-20 16:21:55 -0700

    Create index view php file

---
+++ Views/index.view.php
@@ -0,0 +1,28 @@
+<?php include 'header.view.php'; ?>
+
+<main>
+    <div class="wrapper">
+        <form action="">
+            <img src="images/SAIKYO.png">
+            <div class="input-box">
+                <input type="text" placeholder="username" required>
+                <i class="bx bxs-user"></i>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="password" required>
+                <i class="bx bxs-lock-alt"></i>
+            </div>
+            <div class="remember">
+                <label><input type="checkbox">Remember Me</label>
+                <a href="#">Forget password?</a>
+            </div>
+            <input type="submit" class="btn" value="Login">
+            <div class="register">
+                <p>Don't have an account ? </p>
+                <a href="signup.html"> Register</a>
+            </div>
+        </form>
+    </div>
+</main>
+
+<?php include 'footer.view.php'; ?>
