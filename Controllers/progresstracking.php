e98364a12c8955e87069e22c4283f77eba038a4e
Sri01729 <salahari@northislandcollege.ca>
2023-10-20 17:21:47 -0700

    Update progresstracking controller with php

---
+++ Controllers/progresstracking.php
@@ -0,0 +1,9 @@
+<?php
+// Include the header
+include 'views/header.view.php';
+
+// Load the progress tracking page content
+include 'views/progresstracking.view.php';
+
+// Include the footer
+include 'views/footer.view.php';
