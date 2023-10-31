c3d31019708570e15fc945a46c2e7d17000c1e75
Sri01729 <salahari@northislandcollege.ca>
2023-10-31 04:22:01 -0700

    Add chat views in profile and nutrition tracking page too

--- Controllers/nutritiontracking.php
+++ Controllers/nutritiontracking.php
@@ -5,5 +5,8 @@ include '../views/header.view.php';
 // Load the nutrition tracking page content
 include '../views/nutritiontracking.view.php';
 
+// Include the chat
+include '../views/chat.view.php';
+
 // Include the footer
 include '../views/footer.view.php';
--- Controllers/profile.php
+++ Controllers/profile.php
@@ -5,5 +5,8 @@ include '../views/header.view.php';
 // Load the profile page content
 include '../views/profile.view.php';
 
+// Include the chat
+include '../views/chat.view.php';
+
 // Include the footer
 include '../views/footer.view.php';
