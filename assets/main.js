ef9ec4488e397fea48ea60925141d990fb9f5620
Sri01729 <salahari@northislandcollege.ca>
2023-10-31 03:23:18 -0700

    Implement the sendMessage() function for sending chat messages

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,15 @@
 // Define variables
 const messageInput = document.getElementById('message-input');
 const chatWindow = document.querySelector('.chat-window');
+
+// Function to send a message
+function sendMessage() {
+    const message = messageInput.value;
+    if (message) {
+        const messageElement = document.createElement('div');
+        messageElement.classList.add('message');
+        messageElement.textContent = message;
+        chatWindow.appendChild(messageElement);
+        messageInput.value = '';
+    }
+}
