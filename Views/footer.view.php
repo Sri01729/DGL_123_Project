e012bcbbff57c3df83cd19dab553911bd5f6d481
Sri01729 <salahari@northislandcollege.ca>
2023-10-31 02:55:04 -0700

    Create chat container with chat-window and message in it

--- Views/footer.view.php
+++ Views/footer.view.php
@@ -1,9 +1,16 @@
-        <footer class="footer" style="position: fixed; bottom:0; width:100%">
-            <div class="social-icons">
-                <a href="https://www.facebook.com" class="fa fa-facebook"></a>
-                <a href="https://www.instagram.com" class="fa fa-instagram"></a>
-                <a href="https://www.twitter.com" class="fa fa-twitter"></a>
-            </div>
-        </footer>
-    </body>
+<div class="chat">
+    <div class="chat-window">
+        <div class="message">Message </div>
+    </div>
+</div>
+
+<footer class="footer" style="position: fixed; bottom:0; width:100%">
+    <div class="social-icons">
+        <a href="https://www.facebook.com" class="fa fa-facebook"></a>
+        <a href="https://www.instagram.com" class="fa fa-instagram"></a>
+        <a href="https://www.twitter.com" class="fa fa-twitter"></a>
+    </div>
+</footer>
+</body>
+
 </html>
