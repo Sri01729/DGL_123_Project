e45b0a71b623d36454f15cafad11f02e1918923d
Sri01729 <salahari@northislandcollege.ca>
2023-11-02 11:27:42 -0700

    Update database connection parameters

--- Core/databaseconnection.php
+++ Core/databaseconnection.php
@@ -1,11 +1,11 @@
 <?php
-$type     = 'mysql';
-$server   = 'localhost';
-$db       = 'database_name';
-$port     = '8800';
-$charset  = 'utf8mb4';
-$username = 'username';
-$password = 'password';
+$type     = 'mysql';                 // Type of database
+$server   = '127.0.0.1';             // Server the database is on
+$db       = 'login';                 // Name of the database
+$port     = '3306';                  // Port is 3306 in XAMPP
+$charset  = 'utf8mb4';               // UTF-8 encoding using 4 bytes of data per character
+$username = 'testuser';              // Enter YOUR username here
+$password = 'Cnupavanphp1729@';      // Enter YOUR password here
 
 
 $dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset";
