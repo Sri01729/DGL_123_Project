630f220a559f8ca4b78abeaebcdc7cdc68f17b4c
Sri01729 <salahari@northislandcollege.ca>
2023-11-02 10:15:02 -0700

    Add PDO database connection setup using the provided configuration parameters

--- Core/databaseconnection.php
+++ Core/databaseconnection.php
@@ -6,3 +6,11 @@ $port     = '8800';
 $charset  = 'utf8mb4';
 $username = 'username';
 $password = 'password';
+
+
+$dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset";
+try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), $e->getCode());
+}
