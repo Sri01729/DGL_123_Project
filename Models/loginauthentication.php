a1bc851f7860405f5cc0b0aba340e942dfdbe665
Sri01729 <salahari@northislandcollege.ca>
2023-11-03 13:39:06 -0700

    Add comments to the code

--- Models/loginauthentication.php
+++ Models/loginauthentication.php
@@ -9,14 +9,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $query = $pdo->prepare("SELECT * FROM loginform WHERE username = ?");
     $query ->bindParam(1,$username);
     $query -> execute();
+
+    // Fetch the result as an associative array
     $query_result = $query->fetch(PDO::FETCH_ASSOC);
 
+    // Check if a user with the given username exists
     if($query_result){
+    // Compare the submitted password with the stored password
         if($query_result['password']===$password){
             echo "Login sucessfully";
         } else{
             echo "Invalid username or passsword";
         }
+    }else{
+        echo "Invalid username or passsword";
     }
 
 
