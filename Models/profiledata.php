c7da5327b26234f6f70c3df35d7e33353e47ffba
Sri01729 <salahari@northislandcollege.ca>
2023-11-04 03:13:11 -0700

    Add sql queries to  update name and other data in profile table

--- Models/profiledata.php
+++ Models/profiledata.php
@@ -4,8 +4,19 @@ require '../Core/databaseconnection.php'; // Include the database connection cod
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get profile form data
     $gender = $_POST['gender'];
-    $height = $_POST['name'];
+    $height = $_POST['height'];
     $weight = $_POST['weight'];
     $age = $_POST['age'];
     $bodyfat = $_POST['bodyfat'];
 }
+
+//Update name in profile table based on the email in users table
+$query -> $pdo -> prepare("UPDATE profile AS p
+JOIN users AS u ON p.email = u.email
+SET p.name = u.name;
+");
+
+//Update data in profile table
+$query = $pdo->prepare("UPDATE profile
+        SET gender = :gender, height = :height, weight = :weight, age = :age, body_fat_percent = :body_fat_percent,
+        WHERE name = :name");
