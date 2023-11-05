40b578fa7f1b0dd0e03c1f255fc99d859ed9bbcc
Sri01729 <salahari@northislandcollege.ca>
2023-11-05 01:28:33 -0700

    Edit form attributes in the progress tracking view file

--- Views/progresstracking.view.php
+++ Views/progresstracking.view.php
@@ -6,51 +6,53 @@
         <div class="container">
 
             <div class="flex-container">
-                <div class="grid">
-                    <div class="cell1">
-                        <p>Body Measurements (in cms)</p>
-                    </div>
-                    <div class="cell">
-                        <input type="date">
-                    </div>
-                    <div class="cell">
-                        <input type="text" placeholder="Shoulders" name="shoulders">
-                    </div>
-                    <div class="cell">
-                        <input type="text" placeholder="Neck" name="neck">
-                    </div>
-                    <div class="cell">
-                        <input type="text" placeholder="Forearms" name="forearms">
-                    </div>
-                    <div class="cell">
-                        <input type="text" placeholder="Bust" name="bust">
-                    </div>
-                    <div class="cell">
-                        <input type="text" placeholder="Chest" name="chest">
-                    </div>
-                    <div class="cell">
-                        <input type="text" placeholder="Right Thigh" name="rightThigh">
-                    </div>
-                    <div class="cell">
-                        <input type="text" placeholder="Left Thigh" name="leftThigh">
-                    </div>
-                    <div class="cell">
-                        <input type="text" placeholder="Right Bicep" name="rightBicep">
-                    </div>
-                    <div class="cell">
-                        <input type="text" placeholder="Left Bicep" name="leftBicep">
-                    </div>
-                </div>
-                <div class="chart">
-                    <img src="../assets/images/bodymeasgraph.png">
-                </div>
+                <form action="../Models/progresstracking.php" method="post">
+                    <div class="grid">
+                        <div class="cell1">
+                            <p>Body Measurements (in cms)</p>
+                        </div>
+                        <div class="cell">
+                            <input type="date">
+                        </div>
+                        <div class="cell">
+                            <input type="text" placeholder="Shoulders" name="shoulders">
+                        </div>
+                        <div class="cell">
+                            <input type="text" placeholder="Neck" name="neck">
+                        </div>
+                        <div class="cell">
+                            <input type="text" placeholder="Forearms" name="forearms">
+                        </div>
+                        <div class="cell">
+                            <input type="text" placeholder="Bust" name="bust">
+                        </div>
+                        <div class="cell">
+                            <input type="text" placeholder="Chest" name="chest">
+                        </div>
+                        <div class="cell">
+                            <input type="text" placeholder="Right Thigh" name="rightThigh">
+                        </div>
+                        <div class="cell">
+                            <input type="text" placeholder="Left Thigh" name="leftThigh">
+                        </div>
+                        <div class="cell">
+                            <input type="text" placeholder="Right Bicep" name="rightBicep">
+                        </div>
+                        <div class="cell">
+                            <input type="text" placeholder="Left Bicep" name="leftBicep">
+                        </div>
+                </form>
+            </div>
+            <div class="chart">
+                <img src="../assets/images/bodymeasgraph.png">
             </div>
         </div>
-        <div class="totalmeas">
-            <span>Total Measurements</span>
-            <input class="totalmeassub" type="text">
-        </div>
-        <button class="submit-button">Submit</button>
-    </main>
+</div>
+<div class="totalmeas">
+    <span>Total Measurements</span>
+    <input class="totalmeassub" type="text">
+</div>
+<button class="submit-button">Submit</button>
+</main>
 
-    <?php include 'footer.view.php'; ?>
+<?php include 'footer.view.php'; ?>
