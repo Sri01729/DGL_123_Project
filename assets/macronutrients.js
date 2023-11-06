2c9bd5fd8eefb3645011275d5a1b7befcd6647e0
Sri01729 <salahari@northislandcollege.ca>
2023-11-05 23:56:45 -0800

    Calculate macro nutrients based on specific food group

--- assets/macronutrients.js
+++ assets/macronutrients.js
@@ -22,13 +22,42 @@ function calculateMacroNutrition() {
     var selectedValue3 = selectedOption3.value;
 
     // Define an array to food groups and their corresponding types
-    var valueGroups = {
+    var foodGroups = {
         dairy: ["Panner", "Ice cream"],
         veg: ["Roti", "Rice", "Peanut butter", "Oats"],
         non_veg: ["Beef", "Lamb", "Chicken"],
         snacks: ["Chocolate"]
     };
 
+    var protein;
+    var fat;
+    var carbs;
+    var calories;
+    // Check if the selected value belongs to a specific group
+    for (var group in foodGroups) {
+        if (foodGroups[group].includes(selectedValue1)) {
+
+            // calculate the macro nutrients
+            if (group === "dairy") {
+
+                protein = (20 / 100) * quantity_1;
+                fat = (10 / 100) * quantity_2;
+                carbs = (50 / 100) * quantity_3;
+                calories = (protein * 4) + (carbs * 4) + (fat * 9);
+
+
+
+                break;
+            }else if(group === "veg") {
+
+
+        }else if(group === "non_veg") {
+
+            } else {
+
+        }
+        }
+    }
 
 
 }
