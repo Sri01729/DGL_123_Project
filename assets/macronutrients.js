52d36b51f9c0144f11dbdbc38e3bc1dbedf47275
Sri01729 <salahari@northislandcollege.ca>
2023-11-05 23:28:53 -0800

    Retrieve selected values from dropdown and define an array of food groups

--- assets/macronutrients.js
+++ assets/macronutrients.js
@@ -6,6 +6,29 @@ function calculateMacroNutrition() {
     var quantity_3 = document.getElementById("quantity3").value;
 
 
+    // Get the select element
+    var dropdown1 = document.getElementById("food1");
+    var dropdown2 = document.getElementById("food2");
+    var dropdown3 = document.getElementById("food3");
+
+    // Get the selected option
+    var selectedOption1 = dropdown1.options[dropdown1.selectedIndex];
+    var selectedOption2 = dropdown2.options[dropdown2.selectedIndex];
+    var selectedOption3 = dropdown3.options[dropdown3.selectedIndex];
+
+    // Get the value of the selected option
+    var selectedValue1 = selectedOption1.value;
+    var selectedValue2 = selectedOption2.value;
+    var selectedValue3 = selectedOption3.value;
+
+    // Define an array to food groups and their corresponding types
+    var valueGroups = {
+        dairy: ["Panner", "Ice cream"],
+        veg: ["Roti", "Rice", "Peanut butter", "Oats"],
+        non_veg: ["Beef", "Lamb", "Chicken"],
+        snacks: ["Chocolate"]
+    };
+
 
 
 }
