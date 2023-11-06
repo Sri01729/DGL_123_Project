f70452826506d42a0f3ea2f5a3aea0077ec5715d
Sri01729 <salahari@northislandcollege.ca>
2023-11-05 23:08:25 -0800

    Get the quantity values from the quantity text fields

---
+++ assets/macronutrients.js
@@ -0,0 +1,11 @@
+
+function calculateMacroNutrition() {
+
+    var quantity_1 = document.getElementById("quantity1").value;
+    var quantity_2 = document.getElementById("quantity2").value;
+    var quantity_3 = document.getElementById("quantity3").value;
+
+
+
+
+}
