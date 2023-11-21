43404543e430d7f7f7f5a438d63720914008411f
Sri01729 <salahari@northislandcollege.ca>
2023-11-21 11:50:26 -0800

    Update paths for index.php

--- Controllers/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Include the header
-include '../views/header.view.php';
-
-// Load the main page content
-include '../views/index.view.php';
-
-// Include the footer
-include '../views/footer.view.php';
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// Include the header
+include '../DGL_123_Project/Views/header.view.php';
+
+// Load the main page content
+include '../DGL_123_Project/Views/index.view.php';
+
+// Include the footer/
+include '../DGL_123_Project/Views/footer.view.php';
