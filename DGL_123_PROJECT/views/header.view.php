02f8d6b59c4738da0aff8679c3159b77f953197b
Sri01729 <salahari@northislandcollege.ca>
2023-11-28 10:06:57 -0800

    Extract project root from request uri using $_SERVER super global

--- DGL_123_PROJECT/views/header.view.php
+++ DGL_123_PROJECT/views/header.view.php
@@ -1,6 +1,20 @@
 <?php
+$requestUri = $_SERVER['REQUEST_URI'];
+
+// Find the position of "/DGL_123_PROJECT" in the request URI
+$position = strpos($requestUri, "/DGL_123_PROJECT/");
+
+if ($position !== false) {
+    // Extract "/DGL_123_PROJECT" and everything after it
+    $extractedPath = substr($requestUri, $position, strlen("/DGL_123_PROJECT/"));
+
+    // Output the extracted path
+    $proj_root= $extractedPath;
+} else {
+    // "/DGL_123_PROJECT" not found in the request URI
+    echo "Not found";
+}
 //configuration variable
-$proj_root = "/DGL_123_PROJECT/";
 $navigation_path = "Controllers/";
 $total_path= $proj_root . $navigation_path;
 ?>
