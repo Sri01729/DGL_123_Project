b895858c7c9a19f586059eff4e00fbde009a60a2
Sri01729 <salahari@northislandcollege.ca>
2023-11-28 10:11:38 -0800

    In profile view, Extract project root from request URI using $_server superglobal

--- DGL_123_PROJECT/views/profile.view.php
+++ DGL_123_PROJECT/views/profile.view.php
@@ -1,12 +1,25 @@
 <?php
-//configuration variable
-$proj_root = "/DGL_123_PROJECT/";
+$requestUri = $_SERVER['REQUEST_URI'];
+
+// Find the position of "/DGL_123_PROJECT/" in the request URI
+$position = strpos($requestUri, "/DGL_123_PROJECT/");
+
+if ($position !== false) {
+    // Extract "/DGL_123_PROJECT/" and everything after it
+    $extractedPath = substr($requestUri, $position, strlen("/DGL_123_PROJECT/"));
+
+    // Assign the extracted path
+    $proj_root = $extractedPath;
+} else {
+    // "/DGL_123_PROJECT/" not found in the request URI
+    echo "Not found";
+}
 ?>
 
 <div class="profile">
     <main>
         <h1>Set up profile</h1>
-        <form action="/DGL_123_PROJECT/Models/profiledata.php" method="post">
+        <form action="<?php echo $proj_root; ?>Models/profiledata.php" method="post">
             <div class="form-group">
                 <label class="radio-label">Gender:</label>
                 <div class="radio-group">
