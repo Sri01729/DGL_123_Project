25817cddf78f2166aac444d705f4aafa8072a519
Sri01729 <salahari@northislandcollege.ca>
2023-11-28 10:17:57 -0800

    Create config.php file and add project root extract code in it

--- DGL_123_PROJECT/views/header.view.php
+++ DGL_123_PROJECT/views/header.view.php
@@ -1,19 +1,4 @@
 <?php
-$requestUri = $_SERVER['REQUEST_URI'];
-
-// Find the position of "/DGL_123_PROJECT/" in the request URI
-$position = strpos($requestUri, "/DGL_123_PROJECT/");
-
-if ($position !== false) {
-    // Extract "/DGL_123_PROJECT/" and everything after it
-    $extractedPath = substr($requestUri, $position, strlen("/DGL_123_PROJECT/"));
-
-    // Assign the extracted path
-    $proj_root= $extractedPath;
-} else {
-    // "/DGL_123_PROJECT/" not found in the request URI
-    echo "Not found";
-}
 //configuration variable
 $navigation_path = "Controllers/";
 $total_path= $proj_root . $navigation_path;
