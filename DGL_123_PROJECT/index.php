c14fffa7539b4c5aec76af6abd01a08f3b02c922
Sri01729 <salahari@northislandcollege.ca>
2023-11-28 13:13:44 -0800

    Add PHP error level setting coded

--- DGL_123_PROJECT/index.php
+++ DGL_123_PROJECT/index.php
@@ -1,4 +1,15 @@
 <?php
+// For Production
+error_reporting(0);
+ini_set('display_errors', 0);
+
+// For Test
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+
+
+
+
 // Include the header
 include 'Views/header.view.php';
 
