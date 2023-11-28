cb3e06841df3548dacae084ddbdfdc8b0f8b9e59
Sri01729 <salahari@northislandcollege.ca>
2023-11-28 10:08:37 -0800

    Extract project root from $_server superglobal in index vies

--- DGL_123_PROJECT/views/header.view.php
+++ DGL_123_PROJECT/views/header.view.php
@@ -1,17 +1,17 @@
 <?php
 $requestUri = $_SERVER['REQUEST_URI'];
 
-// Find the position of "/DGL_123_PROJECT" in the request URI
+// Find the position of "/DGL_123_PROJECT/" in the request URI
 $position = strpos($requestUri, "/DGL_123_PROJECT/");
 
 if ($position !== false) {
-    // Extract "/DGL_123_PROJECT" and everything after it
+    // Extract "/DGL_123_PROJECT/" and everything after it
     $extractedPath = substr($requestUri, $position, strlen("/DGL_123_PROJECT/"));
 
-    // Output the extracted path
+    // Assign the extracted path
     $proj_root= $extractedPath;
 } else {
-    // "/DGL_123_PROJECT" not found in the request URI
+    // "/DGL_123_PROJECT/" not found in the request URI
     echo "Not found";
 }
 //configuration variable
