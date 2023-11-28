100d0702a688d0dfc4ce78f771665cc1cf954bba
Sri01729 <salahari@northislandcollege.ca>
2023-11-28 10:09:56 -0800

    Extract project root from request URI using $_server super global in  nutitiontracking view

--- DGL_123_PROJECT/views/nutritiontracking.view.php
+++ DGL_123_PROJECT/views/nutritiontracking.view.php
@@ -1,6 +1,19 @@
 <?php
-//configuration variable
-$proj_root = "/DGL_123_PROJECT/";
+$requestUri = $_SERVER['REQUEST_URI'];
+
+// Find the position of "/DGL_123_PROJECT/" in the request URI
+$position = strpos($requestUri, "/DGL_123_PROJECT/");
+
+if ($position !== false) {
+    // Extract "/DGL_123_PROJECT/" and everything after it
+    $extractedPath = substr($requestUri, $position, strlen("/DGL_123_PROJECT/"));
+
+    // Assign the extracted path
+    $proj_root= $extractedPath;
+} else {
+    // "/DGL_123_PROJECT/" not found in the request URI
+    echo "Not found";
+}
 $imagePath = $proj_root . "assets/images/piechart.png";
 ?>
 <div class="progress">
